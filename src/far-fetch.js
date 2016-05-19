7ea2bdefc89b80fd97209e424bcc09bb5c94bb2a
Rodrigo Navarro <rnavarro1@gmail.com>
2016-05-18 22:55:55 -0300

    Quick™ refactor

--- src/far-fetch.js
+++ src/far-fetch.js
@@ -3,16 +3,22 @@ const makeRequest = ({ url, method, headers, body }) =>
 
 const runFilter = (req, filter) => filter(farFetch({ ...req }));
 
+const bodyless = (req, method) => url =>
+  farFetch({ ...req, url, method });
+
+const bodyful = (req, method) => (url, body) =>
+  farFetch({ ...req, url, body, method });
+
 const farFetch = (req = { filters: [], headers: {} }) => ({
   execute: () => makeRequest(req),
 
   ...req,
 
-  get: url => farFetch({ ...req, url, method: "GET" }),
-  del: url => farFetch({ ...req, url, method: "DELETE" }),
-  post: (url, body) => farFetch({ ...req, url, body, method: "POST" }),
-  put: (url, body) => farFetch({ ...req, url, body, method: "PUT" }),
-  patch: (url, body) => farFetch({ ...req, url, body, method: "PATCH" }),
+  get: bodyless(req, "GET"),
+  del: bodyless(req, "DELETE"),
+  post: bodyful(req, "POST"),
+  put: bodyful(req, "PUT"),
+  patch: bodyful(req, "PATCH"),
 
   set: (header, value) =>
     farFetch({ ...req, headers: { ...req.headers, [header]: value } }),
