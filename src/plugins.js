8ba69940cb9469a567e0f5f070c9efeb76d20833
Rodrigo Navarro <rnavarro1@gmail.com>
2016-05-18 22:02:06 -0300

    Adding delay plugin to demonstrate how we can extend the actual side effectual request function.

--- src/plugins.js
+++ src/plugins.js
@@ -7,3 +7,10 @@ export const logger = (logger = console) => req => {
   logger.log("FarFetch", req.method, req.url);
   return req;
 };
+
+const wait = time => new Promise(resolve => setTimeout(resolve, time));
+
+export const delay = (time = 1000) => req => ({
+  ...req,
+  execute: () => wait(time).then(req.execute)
+});
